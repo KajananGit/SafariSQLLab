c8bcc1f13529a79d2a99b0f59b103327fe8e8e45
Monesha <moneshadhali@gmail.com>
2024-02-21 16:01:13 +0000

    created table template

--- safariLab.sql
+++ safariLab.sql
@@ -1,2 +1,21 @@
--- Made my first change
--- Made my 2nd change from local computer
+DROP TABLE IF EXISTS assignments;
+DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS enclosures;
+DROP TABLE IF EXISTS staffs;
+
+CREATE TABLE staffs(
+    
+);
+
+CREATE TABLE enclosures(
+
+);
+
+CREATE TABLE animals(
+
+);
+
+
+CREATE TABLE assignments(
+
+);
