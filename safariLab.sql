3ff5031464c361f143d325760174d41c775469a4
Monesha <moneshadhali@gmail.com>
2024-02-21 16:14:05 +0000

    tables created

--- safariLab.sql
+++ safariLab.sql
@@ -4,18 +4,30 @@ DROP TABLE IF EXISTS enclosures;
 DROP TABLE IF EXISTS staffs;
 
 CREATE TABLE staffs(
-    
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    employeeNumber INT
 );
 
 CREATE TABLE enclosures(
-
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    capacity INT,
+    closedForMaintenance BOOLEAN
 );
 
 CREATE TABLE animals(
-
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    type VARCHAR(255),
+    age INT,
+    enclosure_id INT REFERENCES enclosures(id)
 );
 
 
 CREATE TABLE assignments(
-
+    id SERIAL PRIMARY KEY,
+    employee_id INT REFERENCES staffs(id),
+    enclosure_id INT REFERENCES enclosures(id),
+    day VARCHAR(255)
 );
